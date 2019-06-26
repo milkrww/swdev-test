342967cabcee38482b9c14b343894ab22e596433
milkrww <honey.milky.912@gmail.com>
2019-06-26 18:41:27 +0700

    Create home page

---
+++ lab1-hello/src/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+	public static void main(String args[])
+	{
+//		int a = 10;
+//		double price = 10.5;
+//		String name = "Milk";
+//		
+//		for(int i=0 ; i<5 ; i++)
+//			System .out.println(name);
+//		
+//		int[] num = {1,2,3,4,9,10};
+//		for(int i=0 ; i<num.length ; i++)
+//			System.out.println(num[i]);
+//		
+//		int[] num2 = new int[100];
+//		
+//		String[] fruit = {"banana","apple"};
+//		for(String f : fruit)
+//			System.out.println(f);
+		
+		String tel = "053-211-222";
+		String[] ts = tel.split("-");
+		for(String t : ts)
+			System.out.println(t);
+		
+	
+	
+	
+	}
+	
+}
